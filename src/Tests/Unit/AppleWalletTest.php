7e86a7d0ff84ca553ae9650a5851c2f539f68ae6
Andreas Reichel <a.reichel@schubwerk.de>
2024-12-19 15:12:44 +0100

    feat(google)[TWOH-000]: added unit tests

---
+++ src/Tests/Unit/AppleWalletTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace TWOH\WalletDriver\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+use TWOH\WalletDriver\Exceptions\ValidationFailedException;
+use TWOH\WalletDriver\Models\Account;
+use TWOH\WalletDriver\Models\Wallet;
+use TWOH\WalletDriver\Models\WalletStyle;
+use TWOH\WalletDriver\Services\WalletDriverService;
+
+class AppleWalletTest extends TestCase
+{
+    /**
+     * @throws ReflectionException
+     * @throws ValidationFailedException
+     */
+    public function testGeneratedAppleWalletUrlUsingRealObjects(): void
+    {
+        $account = new Account(
+            '',
+            'Apple',
+            'Application',
+            '',
+            '',
+            '',
+            '../Certificates.p12',
+            'password',
+            '/var/www/html/pkpass/',
+            'pass.com.yourcompany.loyalty',
+            'KN44X8ZLNC'
+        );
+
+        $walletStyle = new WalletStyle(
+            'https://www.example.com/images/logo.png',
+            'https://www.example.com/images/background.png',
+            'https://www.example.com/images/icon.png',
+            'rgb(32,110,247)',
+            'rgb(255,255,255)'
+        );
+
+        $wallet = new Wallet(
+            '',
+            '',
+            'Beispiel-Unternehmen',
+            'Beispiel-Treueprogramm',
+            'active',
+            [
+                'accountId' => '123456789',
+                'accountName' => 'Max Mustermann',
+                'barcode' => [
+                    'type' => 'PKBarcodeFormatPDF417',
+                    'value' => '1234ABC5678',
+                    'alternateText' => 'Scan mich!'
+                ]
+            ],
+            $walletStyle
+        );
+
+        $walletDriverService = new WalletDriverService($account, $wallet);
+        $generatedAppleWalletUrl = $walletDriverService->__invoke();
+
+        $this->assertNotNull($generatedAppleWalletUrl);
+        $this->assertIsString($generatedAppleWalletUrl);
+    }
+}
---
+++ src/Tests/Unit/GoogleWalletTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace TWOH\WalletDriver\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+use TWOH\WalletDriver\Exceptions\ValidationFailedException;
+use TWOH\WalletDriver\Models\Account;
+use TWOH\WalletDriver\Models\Wallet;
+use TWOH\WalletDriver\Models\WalletStyle;
+use TWOH\WalletDriver\Services\WalletDriverService;
+
+class GoogleWalletTest extends TestCase
+{
+    /**
+     * @throws ReflectionException
+     * @throws ValidationFailedException
+     */
+    public function testGeneratedGoogleWalletUrlUsingRealObjects(): void
+    {
+        $account = new Account(
+            'your-issuer-id',
+            'Google',
+            'Application',
+            '/path/to/your-service-account.json',
+            'https://www.googleapis.com/auth/wallet_object.issuer',
+            '/path/to/private-key.pem',
+            '',
+            '',
+            '',
+            '',
+            ''
+        );
+
+        $walletStyle = new WalletStyle(
+            'https://www.example.com/images/logo.png',
+            'https://www.example.com/images/background.png',
+            '',
+            '#4285F4',
+            '#FFFFFF',
+        );
+
+        $wallet = new Wallet(
+            'yourIssuerId.loyaltyClass1',
+            'yourIssuerId.loyaltyObject1',
+            'Beispiel-Unternehmen',
+            'Beispiel-Treueprogramm',
+            'active',
+            [
+                'accountId' => '123456789',
+                'accountName' => 'Max Mustermann',
+                'barcode' => [
+                    'type' => 'qrCode',
+                    'value' => '1234ABC5678',
+                    'alternateText' => 'Scan mich!'
+                ]
+            ],
+            $walletStyle
+        );
+
+        $walletDriverService = new WalletDriverService($account, $wallet);
+        $generatedGoogleWalletUrl = $walletDriverService->__invoke();
+
+        $this->assertNotNull($generatedGoogleWalletUrl);
+        $this->assertIsString($generatedGoogleWalletUrl);
+    }
+}
